a1af8fae6bf8d0cb2fefc1297a24bd148dd79c09
Gizmoux <nicolas.deriemacker@hotmail.fr>
2025-02-17 17:23:51 +0100

    ✅ Add CoursesPage

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,20 +1,29 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import Link from "next/link";
 
 const CoursesPage = () => {
+  const courses = [
+    { id: "1", name: "BeginJavascript" },
+    { id: "2", name: "BeginReact" },
+    { id: "3", name: "BeginPython" },
+    { id: "4", name: "BeginDjango" },
+  ];
+
   return (
     <div>
-      <h1>Courses</h1>
-      <Button>New Course</Button>
+      <h1>Courses Page</h1>
+      <Link href="/admin/courses">Back to Courses</Link>
+      <button>New Courseeee</button>
 
       <h2>Image</h2>
       <div>
         <h2>Name</h2>
         <ul>
-          <li>BeginJavascript</li>
-          <li>BeginReact</li>
-          <li>BeginPython</li>
-          <li>BeginDjango</li>
+          {courses.map((course) => (
+            <li key={course.id}>
+              {/* Lien vers la page dynamique de chaque cours */}
+              <Link href={`/admin/courses/${course.id}`}>{course.name}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
